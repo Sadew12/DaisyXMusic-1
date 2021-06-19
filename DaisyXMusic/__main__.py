589373bd3e670be28b9677ec3cd7c535abea1899
Sadew12 <83544209+Sadew12@users.noreply.github.com>
2021-06-19 14:24:40 +0530

    Update __main__.py

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,5 +1,4 @@
-# DaisyXMusic (Telegram bot project)
-# Copyright (C) 2021  Inuka Asith & Rojserbest
+
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
